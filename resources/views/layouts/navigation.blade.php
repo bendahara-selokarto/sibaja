9d8b87217fbc0d751fdb1980881a374933a639b5
harmanto <bendaharadesaselokarto@gmail.com>
2025-02-24 18:09:31 +0700

    change responsive nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -91,6 +91,16 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('menu.penyedia')" :active="request()->routeIs('menu.penyedia')">
+                {{ __('Penyedia') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('menu.kegiatan')" :active="request()->routeIs('menu.kegiatan')">
+                {{ __('Kegiatan') }}
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
