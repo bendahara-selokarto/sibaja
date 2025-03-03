4e482db7002394e5bab24442da33c5f7e08ed2c4
harmanto <bendaharadesaselokarto@gmail.com>
2025-03-03 08:24:24 +0700

    memperbaiki typedata kolom penyedia di kolom penawaran harga

--- database/migrations/2025_01_26_043556_create_penawaran_hargas_table.php
+++ database/migrations/2025_01_26_043556_create_penawaran_hargas_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->foreignUuid('kegiatan_id')->constrained()->cascadeOnDelete();
             $table->dateTime('tgl_penawaran_1')->nullable();
             $table->dateTime('tgl_penawaran_2')->nullable();
-            $table->dateTime('penyedia_1')->nullable();
-            $table->dateTime('penyedia_2')->nullable();
+            $table->foreignUuid('penyedia_1')->nullable();
+            $table->foreignUuid('penyedia_2')->nullable();
             $table->string('item_penawaran_1')->nullable();
             $table->string('item_penawaran_2')->nullable();
             $table->string('harga_penawaran_1')->nullable();
