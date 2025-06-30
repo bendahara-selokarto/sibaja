f6414eea9aeca2f641e8533f8990b5c577327322
harmanto <bendaharadesaselokarto@gmail.com>
2025-06-30 23:30:04 +0700

    pembatasan ijin penggunaan

--- app/Http/Middleware/CheckDefaultKode.php
+++ app/Http/Middleware/CheckDefaultKode.php
@@ -15,8 +15,33 @@ class CheckDefaultKode
      */
     public function handle(Request $request, Closure $next)
     {
+        /**
+         * Middleware untuk memeriksa apakah pengguna telah mengisi kode desa pada profilnya.
+         * Jika pengguna sudah login (authenticated) namun field 'kode_desa' masih null,
+         * maka pengguna akan diarahkan ke halaman edit profil dengan pesan error
+         * untuk mengisi profil desa terlebih dahulu.
+         *
+         * @return \Illuminate\Http\RedirectResponse|null Redirect ke halaman edit profil jika 'kode_desa' belum diisi, null jika sudah.
+         */
         if (Auth::check() && Auth::user()->kode_desa == null) {
             return redirect()->route('profile.edit')->with('error', 'Silakan isi profil desa terlebih dahulu!');
+        }
+        // Daftar pengguna yang diijinkan
+        $allowedUsers = [
+            'selokarto',
+            'gemuh',
+            'randu',
+            'Selokarto',
+            'Gemuh',
+            'Randu',
+            'SELOKARTO',
+            'GEMUH',
+            'RANDU',
+            // Tambahkan email pengguna lain yang diijinkan di sini
+        ];
+
+        if (Auth::check() && !in_array(Auth::user()->desa, $allowedUsers)) {
+            return redirect()->route('profile.edit')->with('error', 'Hubungi admin untuk penggunaan sibaja!');
         }
 
         return $next($request);
