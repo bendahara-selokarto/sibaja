99c0ab4c72a31342fe7659971324028f89060527
harmanto <bendaharadesaselokarto@gmail.com>
2025-07-09 05:44:34 +0700

    menambahkan fromat surat-perintah-kerja dan surat-perjanjian-kerja pdf

--- resources/views/pdf/negosiasi-harga/surat-perintah-kerja.blade.php
+++ resources/views/pdf/negosiasi-harga/surat-perintah-kerja.blade.php
@@ -1,21 +1,145 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>        
-        body.surat-perintah-kerja {
-          margin-left:40pt;
-        }
-        body.surat-perintah-kerja h3 {
-          text-align: center;
+    <title>Surat Perintah Kerja</title>
+    <style>
+        table {
+            border-collapse: collapse;
         }
     </style>
 </head>
-<body class="surat-perintah-kerja">
-    <x-kop-desa/>
-    <h3>SURAT PERINTAH KERJA</h3>    
+<body style="margin: 0px 20px 20px 80px ;">
+    <x-kop-desa></x-kop-desa>
+    <div style="text-align: center; line-height: 1;">
+        <strong><h2 style="margin-bottom: 0px;">SURAT PERINTAH KERJA</h2></strong><br>
+        NOMOR: ........../SPK/…………./………./………..
+    </div>
+    
+    <p>Pada hari ……………. Tanggal …………….. Bulan ………….. Tahun ........................., kami yang bertanda tangan di bawah ini:</p>
+
+    <table class="no-border">
+        <tr>
+            <td><strong>Nama</strong></td>
+            <td>: .......................</td>
+        </tr>
+        <tr>
+            <td><strong>Jabatan</strong></td>
+            <td>: Pelaksana Kegiatan Anggaran</td>
+        </tr>
+        <tr>
+            <td><strong>Alamat</strong></td>
+            <td>: ........................</td>
+        </tr>
+    </table>
+    <p>Selanjutnya disebut sebagai <strong>PIHAK PERTAMA</strong></p>
+
+    <table class="no-border">
+        <tr>
+            <td><strong>Nama</strong></td>
+            <td>: .......................</td>
+        </tr>
+        <tr>
+            <td><strong>Jabatan</strong></td>
+            <td>: .......................</td>
+        </tr>
+        <tr>
+            <td><strong>Alamat</strong></td>
+            <td>: ........................</td>
+        </tr>
+    </table>
+    <p>Selanjutnya disebut sebagai <strong>PIHAK KEDUA</strong></p>
+
+    <ol>
+        <li>PIHAK KEDUA menyatakan bersedia melaksanakan perintah kerja dari PIHAK PERTAMA untuk pekerjaan:</li>
+    </ol>
+
+    <table style="width: 93%; margin-left:auto">
+        <thead>
+            <tr>
+                <th style="border: 1px solid black;">No</th>
+                <th style="border: 1px solid black;">Jenis Pekerjaan <br> yang Dikerjakan</th>
+                <th style="border: 1px solid black;">Volume</th>
+                <th style="border: 1px solid black;">Harga Satuan <br>(Rp)</th>
+                <th style="border: 1px solid black;">Harga <br>(Rp)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="border: 1px solid black;">1</td>
+                <td style="border: 1px solid black;">………..</td>
+                <td style="border: 1px solid black;">…………..</td>
+                <td style="border: 1px solid black;">………………..</td>
+                <td style="border: 1px solid black;">…………………</td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid black;" colspan="4" ><strong>Jumlah</strong></td>
+                <td style="border: 1px solid black;" >……………………</td>
+            </tr>
+        </tbody>
+    </table>
+
+    
+    <ol start="2">
+        <li>PIHAK KESATU dalam jabatan tersebut di atas memberikan pekerjaan kepada PIHAK KEDUA dan PIHAK KEDUA menyetujui ikatan kerja sebagai pelaksana kerja (Pemborong) dengan Kesepakatan Upah Kerja untuk melaksanakan pekerjaan tersebut.</li>
+        <li>Kegiatan:<br>………………………………………………………………………………………………..</li>
+        <li>Jangka Waktu Pelaksanaan
+            <table style="width: 100%;">
+                <tr>
+                    <td style="vertical-align:top; width:fit-content;">4.1 </td>
+                    <td style="width:auto">Jangka waktu pelaksanaan kontrak pekerjaan ………… (………………….) hari kalender terhitung mulai tanggal, ……………….s/d ……………….. </td>
+                    
+                </tr>
+                <tr>
+                    <td style="vertical-align:top; width:fit-content;">4.2 </td>
+                    <td>Pekerjaan tersebut harus diselesaikan dengan baik oleh PIHAK KEDUA sampai pada batas waktu yang telah disepakati bersama-sama, selambat-lambatnya tanggal, ……………………..</td>
+                </tr>
+            </table>
+        </li>
+        <li>Harga Upah Kerja dan Cara Pembayaran
+            <table>
+                <tr>
+                    <td style="width: fit-content; vertical-align:top">5.1 </td>
+                    <td>Harga upah kerja untuk melaksanakan pekerjaan ………………………… di wilayah Desa …………………………. berdasarkan hasil negosiasi ditetapkan sebesar Rp.......................................................………………….. (……………………………………………………………………………………...)</td>
+                </tr>
+                <tr>
+                    <td style="width: fit-content; vertical-align:top">5.2 </td>
+                    <td>Cara pembayaran/upah kerja secara sekaligus (borongan) atau setelah prestasi pekerjaan mencapai 100% dengan ketentuan dilengkapi dengan Berita Acara Pemeriksaan Pekerjaan dan Berita Acara Serah Terima Pekerjaan, dibayarkan melalui Bendahara Desa ……………………….</td>
+                </tr>
+            </table>
+        </li>
+        <li>Syarat-syarat Pelaksanaan Pekerjaan
+            <table>
+                <tr>
+                    <td style="width: fit-content; vertical-align:top">6.1 </td>
+                    <td>PIHAK KEDUA harus melaksanakan pekerjaan serta mengikuti petunjuk teknis dari PIHAK PERTAMA.</td>
+
+                </tr>
+                <tr>
+                    <td style="width: fit-content; vertical-align:top">6.2 </td>
+                    <td>Alat dan bahan pekerjaan tersebut disediakan oleh PIHAK KEDUA.</td>
+                </tr>
+            </table>
+        </li>
+    </ol>
+
+    <p>Surat Perintah Kerja ini berlaku selama yang menerima pekerjaan melaksanakan pekerjaan sampai dengan selesai dan hasil pekerjaan diserahterimakan oleh PIHAK KEDUA kepada PIHAK KESATU dengan baik.</p>
+
+    <p>Demikian Surat Perintah Kerja untuk Upah Kerja ini dibuat dan berlaku sejak tanggal dikeluarkannya dan ditandatangani oleh kedua belah pihak.</p>
+
+    <table style="text-align: center;">
+        <tr>
+            <td>
+                PIHAK KEDUA,<br><br><br><br><br>
+                (…………………………………….)
+            </td>
+            <td>
+                PIHAK PERTAMA,<br>
+                PELAKSANA KEGIATAN ANGGARAN<br><br><br><br>
+                (…………………………)
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
+
--- resources/views/pdf/negosiasi-harga/surat-perjanjian-kerja.blade.php
+++ resources/views/pdf/negosiasi-harga/surat-perjanjian-kerja.blade.php
@@ -1,21 +1,142 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        body.surat-perjanjian-kerja {
-            margin-left: 40pt;
-        }
-        body.surat-perjanjian-kerja h3{
-            text-align: center;
-        }
+    <title>Surat Perjanjian Kerja</title>    
     </style>
-    <title>Document</title>
 </head>
-<body class="surat-perjanjian-kerja">
-    <x-kop-desa/>
-    <h3>SURAT PERJANJIAN KERJA</h3>    
+<body style="margin: 0px 20px 20px 80px ;">
+    <x-kop-desa></x-kop-desa>
+
+<h2 style="text-align: center; margin-bottom: 0px">SURAT PERJANJIAN KERJA</h2>
+<p style="margin-top: 0px; text-align:center">NOMOR: / / SPK /<br>TANGGAL:</p>
+
+<p>Pada hari ini ………….. tanggal …………… bulan ……….. tahun ............... Kami yang bertanda tangan di bawah ini:</p>
+
+<ol type="1">
+    <li>
+        Nama: …………………….<br>
+        Jabatan: Pelaksana Kegiatan Anggaran<br>
+        Alamat: …………………….<br>
+        <em>Selanjutnya disebut PIHAK PERTAMA</em>
+    </li>
+    <li>
+        Nama: …………………….<br>
+        Jabatan: …………………….<br>
+        Alamat: …………………….<br>
+        <em>Selanjutnya disebut PIHAK KEDUA</em>
+    </li>
+</ol>
+
+<div class="pasal">Pasal 1 – TUGAS PEKERJAAN</div>
+<p>PIHAK PERTAMA memberikan pekerjaan kepada PIHAK KEDUA dan PIHAK KEDUA menerima dan menyatakan bersedia melaksanakan pekerjaan:</p>
+<ul>
+    <li>Jenis pekerjaan: ……………………. </li>
+    <li>Lokasi pekerjaan: ……………………. </li>
+</ul>
+
+<div class="pasal">Pasal 2 – NILAI PEKERJAAN</div>
+<p>Nilai pekerjaan yang disepakati oleh kedua pihak sebesar:</p>
+
+<table style="width: 100%">
+    <thead>
+        <tr>
+            <th style="border-color:1px solid black">No</th>
+            <th style="border-color:1px solid black">Jenis Pekerjaan</th>
+            <th style="border-color:1px solid black">Banyaknya</th>
+            <th style="border-color:1px solid black">Satuan (Rp)</th>
+            <th style="border-color:1px solid black">Jumlah (Rp)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr><td colspan="5" style="height: 100px;"></td></tr>
+        <tr>
+            <td colspan="4"><strong>Jumlah</strong></td>
+            <td>Rp ………………….</td>
+        </tr>
+    </tbody>
+</table>
+
+<div class="pasal">Pasal 3 – JANGKA WAKTU PELAKSANAAN</div>
+<p>Jangka waktu pelaksanaan adalah selama ............. (................) hari kalender sejak tanggal …………………., dan harus diselesaikan paling lambat tanggal ………………….</p>
+
+<div class="pasal">Pasal 4 – SERAH TERIMA PEKERJAAN</div>
+<ol type="1">
+    <li>Setelah pekerjaan selesai 100%, penyedia barang mengajukan penyerahan kepada PKA secara tertulis.</li>
+    <li>Pejabat pemeriksa melakukan pemeriksaan maksimal 7 hari setelah pengajuan.</li>
+</ol>
+<p>Jika terdapat kekeliruan saat serah terima, PIHAK KEDUA wajib memperbaiki sesuai dokumen penunjukan langsung.</p>
+
+<div class="pasal">Pasal 5 – CARA PEMBAYARAN</div>
+<ol type="1">
+    <li>Pembayaran 100% dilakukan setelah pekerjaan selesai dan dilampiri berita acara.</li>
+    <li>Pembayaran dilakukan melalui DPA Desa ………… untuk belanja modal pengadaan ……………. sebesar Rp ………………. ( ………………. ) dipotong pajak sesuai ketentuan.</li>
+</ol>
+
+<div class="pasal">Pasal 6 – HAK DAN KEWAJIBAN</div>
+<p><strong>Tim Pengelola Kegiatan:</strong></p>
+<ul>
+    <li>Mengawasi pekerjaan.</li>
+    <li>Meminta laporan periodik.</li>
+    <li>Menangguhkan pembayaran & mengenakan denda.</li>
+    <li>Memberikan instruksi & membayar nilai SPK.</li>
+</ul>
+
+<p><strong>Penyedia Barang:</strong></p>
+<ul>
+    <li>Menerima pembayaran sesuai nilai SPK.</li>
+    <li>Melaksanakan pekerjaan tepat waktu dan melaporkan secara periodik.</li>
+    <li>Menyerahkan hasil pekerjaan sesuai jadwal.</li>
+</ul>
+
+<div class="pasal">Pasal 7 – SANKSI DAN DENDA</div>
+<ol type="1">
+    <li>Denda dikenakan jika terjadi keterlambatan pekerjaan.</li>
+    <li>Besarnya denda: 1/1000 dari nilai SPK per hari keterlambatan.</li>
+</ol>
+
+<div class="pasal">Pasal 8 – KEADAAN KAHAR (FORCE MAJEURE)</div>
+<ul>
+    <li>Termasuk: bencana, peraturan pemerintah, huru-hara, dsb.</li>
+    <li>Penyedia wajib melapor dalam 14 hari sejak kejadian dengan bukti otoritas.</li>
+    <li>Tanpa laporan, keterlambatan tidak dianggap force majeure.</li>
+    <li>Permintaan perpanjangan harus dijawab dalam 7 hari oleh Tim.</li>
+</ul>
+
+<div class="pasal">Pasal 9 – PENGHENTIAN DAN PEMUTUSAN SPK</div>
+<ol type="1">
+    <li>SPK dihentikan jika pekerjaan selesai atau karena force majeure.</li>
+    <li>SPK diputus jika penyedia cidera janji, kolusi, atau korupsi.</li>
+</ol>
+
+<div class="pasal">Pasal 10 – PENYELESAIAN PERSELISIHAN</div>
+<ol type="1">
+    <li>Diselesaikan secara musyawarah.</li>
+    <li>Jika gagal, maka diselesaikan melalui Pengadilan Negeri Tanah Paser.</li>
+</ol>
+
+<div class="pasal">Pasal 11 – KETENTUAN LAIN-LAIN</div>
+<ol type="1">
+    <li>Biaya administrasi dan materai ditanggung PIHAK KEDUA.</li>
+    <li>SPK dibuat 4 rangkap, 2 asli bermaterai.</li>
+</ol>
+
+<div class="pasal">Pasal 12 – PENUTUP</div>
+<p>Demikian Surat Perintah Kerja ini dibuat dan ditandatangani pada tanggal yang telah ditetapkan untuk dipergunakan sebagaimana mestinya.</p>
+
+<table class="signature">
+    <tr>
+        <td>
+            PIHAK KEDUA<br><br><br>
+            (TOKO/UD/CV ....................)<br><br><br>
+            ..................................................
+        </td>
+        <td>
+            PIHAK KESATU<br>PKA<br><br><br><br>
+            ..................................................
+        </td>
+    </tr>
+</table>
+
 </body>
 </html>
