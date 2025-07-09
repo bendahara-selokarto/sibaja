d6a9ce1b46705b2ff6e5c7e0e760b88d1df2535c
harmanto <bendaharadesaselokarto@gmail.com>
2025-07-09 19:46:53 +0700

    menyelesaikan pdf A2

--- resources/views/pdf/pembayaran/A2.blade.php
+++ resources/views/pdf/pembayaran/A2.blade.php
@@ -1,14 +1,135 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Tanda Bukti Pengeluaran</title>
+    <style>
+        
+    </style>
 </head>
-<body style="margin: 0 20px 20px 60px">
-    <P>form-A2</P>
-    <h3>BUKTI PENERIMAAN PEMBAYARAN</h3>
+<body style="margin: 50px 50px 300px 50px; line-height: 1; vertical-align:text-top; ">
+    <div style="border: 2px solid black">
+    <table style="width: 100%; border-bottom: 1px solid black; margin:0px">        
+        <tr>
+            <td rowspan="5" style="width: 124px">IMG</td>
+            <td colspan="4" style="text-transform: uppercase; font-weight: bold">
+                PEMERINTAH KABUPATEN BATANG               
+            </td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold; width:120pt">KECAMATAN</td>
+            <td style="font-weight: bold; width:10pt;">: </td>
+            <td style="font-weight: bold; width:auto; text-transform: uppercase; ">PECALUNGAN</td>
+            <td style="width: auto">No: </td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold">DESA</td>
+            <td style="font-weight: bold">: </td>
+            <td style="font-weight: bold; text-transform: uppercase;">SELOKARTO</td>
+            <td>Lembar ke:</td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold;">TAHUN ANGGARAN</td>
+            <td style="font-weight: bold;">: </td>
+            <td style="font-weight: bold;"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="font-weight: bold;">SUMBER DANA</td>
+            <td style="font-weight: bold;">: </td>
+            <td style="font-weight: bold; text-transform: uppercase;"></td>
+            <td style="border-bottom: 1px solid black"></td>
+        </tr>
+    </table>
+<table style="width: 100%; margin:0px">
+    <tr>
+        <td style="width: 58%; vertical-align: top; padding-right: 20px;">
+            {{-- Kiri --}}
+            <h3 style="text-align: center; text-decoration: underline; margin-top: 30px;">TANDA BUKTI PENGELUARAN</h3>
+            <table style="width: 100%; margin-top: 20px;">
+                <tr>
+                    <td style="vertical-align: top; padding: 5px;">Sudah menerima dari</td>
+                    <td style="vertical-align: top; width: 4pt; padding: 5px;">:</td>
+                    <td style="vertical-align: top; padding: 5px;">Bendahara Desa</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; padding: 5px;">Uang Sejumlah</td>
+                    <td style="vertical-align: top; width: 4pt; padding: 5px;">:</td>
+                    <td style="vertical-align: top; padding: 5px;">
+                        <strong>Rp 61.321.000</strong><br>
+                        <em>enam puluh satu juta tiga ratus dua puluh satu ribu rupiah</em>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; padding: 5px;">Yaitu Untuk Pembayaran</td>
+                    <td style="vertical-align: top; width: 4pt; padding: 5px;">:</td>
+                    <td style="vertical-align: top; padding: 5px;">Belanja Material</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top; width: 45%; padding: 5px;">Untuk Pekerjaan/ Kegiatan</td>
+                    <td style="vertical-align: top; width: 4pt; padding: 5px;">:</td>
+                    <td style="vertical-align: top; padding: 5px;">Pengaspalan Jalan RT 02 (Endrokilo)</td>
+                </tr>
+            </table>
+        </td>
+        
+        <td style="width: 42%; vertical-align: top; padding-left: 20px; border: 1px solid black;" cellpadding="2">
+            {{-- Kanan --}}
+
+            
+            <div style="text-align: left; margin-top: 30px;">
+                Barang-barang dimaksud telah dibukukan ke buku persediaan/inventaris pada tanggal:
+            </div>
+            <hr>
+
+            <table style="width: 100%; margin-top: 10px;" cellspacing="0" cellpadding="2">
+                <tr>
+                    <td>Jumlah Kotor</td>
+                    <td>Rp 61.321.000</td>
+                </tr>
+                <tr>
+                    <td>Potongan</td>
+                    <td>Rp 6.905.518</td>
+                </tr>
+                <tr>
+                    <td>Dibayarkan</td>
+                    <td>Rp 54.415.482</td>
+                </tr>
+            </table>
+
+            <div style="margin-top: 10px;"><strong>Perincian Potongan :</strong></div>
+            <table style="width: 100%; margin-top: 5px;">
+                <tr><td>1. PPh 21</td><td>Rp -</td></tr>
+                <tr><td>2. PPh 22</td><td>Rp 828.662</td></tr>
+                <tr><td>3. PPh 23</td><td>Rp -</td></tr>
+                <tr><td>4. PPN</td><td>Rp 6.076.856</td></tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+
+<div style="text-align: center; margin-left:auto; width:300px">
+    <p style="margin-top: 30px;">Gemuh, 17 Juni 2025</p>
+    <p>Yang menerima</p>
+
+</div>
+
+    <div style="border: 2px solid black; display: inline-block; padding: 10px 20px; margin: 20px;">
+        <strong style="font-size: 18px;">LUNAS DIBAYAR</strong><br>
+        <span style="font-style: italic;">17 Juni 2025</span>
+    </div>
+
+    
+
+    <table style="width: 100%; text-align: center; margin-top: 40px;">
+        <tr>
+            <td style="border: 1px solid black" >Setuju dibayarkan<br>Kepala Desa Gemuh <br><br><br><br><br></td>
+            <td style="border: 1px solid black" >Telah di Verifikasi<br>Sekretaris Desa <br><br><br><br><br></td>
+            <td style="border: 1px solid black" >Telah dibayarkan<br>Kaur Keuangan <br><br><br><br><br></td>
+            <td style="border: 1px solid black" >Pelaksana Kegiatan<br>Kasi Kesejahteraan <br><br><br><br><br></td>
+        </tr>      
+    </table>
     
 </body>
 </html>
