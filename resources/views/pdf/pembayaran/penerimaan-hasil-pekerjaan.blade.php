a243899a1a074911a0d7deb0070a39b053494624
harmanto <bendaharadesaselokarto@gmail.com>
2025-07-09 07:06:16 +0700

    menambahkan pdf penerimaan-hasil-pekerjaan

--- resources/views/pdf/pembayaran/penerimaan-hasil-pekerjaan.blade.php
+++ resources/views/pdf/pembayaran/penerimaan-hasil-pekerjaan.blade.php
@@ -1,1 +1,74 @@
-<h3>BERITA ACARA PENERIMAAN HASIL PEKERJAAN</h3>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Berita Acara Penerimaan Hasil Pekerjaan</title>
+</head>
+<body style="font-family: serif; line-height: 1.6; font-size: 14px; margin: 40px 40px 40px 100px;">
+
+    <h2 style="text-align: center; text-transform: uppercase;">BERITA ACARA PENERIMAAN HASIL PEKERJAAN</h2>
+    <p style="text-align: center;">NOMOR ……………………</p>
+
+    <p>Pada hari ini …………… tanggal ……….. bulan ………… tahun …….. bertempat di ………, telah dilaksanakan pembayaran atas pekerjaan ………… antara :</p>
+
+    <p><strong>I. PIHAK PERTAMA</strong></p>
+    <table style="margin-left: 20px;">
+        <tr>
+            <td style="width: 120px;">Nama</td>
+            <td>: …………………………</td>
+        </tr>
+        <tr>
+            <td>Jabatan</td>
+            <td>: …………………………</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: …………………………</td>
+        </tr>
+    </table>
+
+    <p><strong>II. PIHAK KEDUA</strong></p>
+    <table style="margin-left: 20px;">
+        <tr>
+            <td style="width: 120px;">Nama</td>
+            <td>: …………………………</td>
+        </tr>
+        <tr>
+            <td>Jabatan</td>
+            <td>: …………………………</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: …………………………</td>
+        </tr>
+    </table>
+
+    <p>PIHAK PERTAMA menyatakan bahwa telah menerima hasil pekerjaan berupa ………………. dalam keadaan baik dari PIHAK KEDUA sesuai dengan Surat Perjanjian Nomor: ………………… dan Nomor ………….. tertanggal ……………….</p>
+
+    <p>PIHAK KEDUA telah menyerahkan hasil pekerjaan berupa ………… dalam keadaan baik kepada PIHAK PERTAMA sesuai dengan Surat Perjanjian Nomor ……… dan Nomor ……… tertanggal ……………….</p>
+
+    <p>Demikian Berita Acara ini dibuat rangkap 2 (dua) masing-masing bermeterai cukup dan mempunyai kekuatan hukum yang sama untuk dipertanggungjawabkan sesuai peraturan perundang-undangan yang berlaku.</p>
+
+    <br><br>
+
+    <table style="width: 100%; text-align: center;">
+        <tr>
+            <td>Direktur/Pimpinan/Pemilik</td>
+            <td>Pelaksana Kegiatan Anggaran</td>
+        </tr>
+        <tr>
+            <td style="padding-top: 60px;">………………………….</td>
+            <td style="padding-top: 60px;">………………………….</td>
+        </tr>
+    </table>
+
+    <br><br>
+
+    <p>Mengetahui</p>
+    <p>Kepala Desa …………….</p>
+    <p>Selaku Pemegang Kekuasaan Pengelolaan Keuangan Desa</p>
+    <br><br>
+    <p style="text-align: left;">……………………………………………</p>
+
+</body>
+</html>
