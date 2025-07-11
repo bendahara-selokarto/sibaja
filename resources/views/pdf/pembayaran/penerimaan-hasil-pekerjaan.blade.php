27dc038c50d87234fe5e238fe906481b9e462b0c
bendahara-selokarto <bendaharadesaselokarto@gmail.com>
2025-07-12 03:57:36 +0700

    isi penerimaan hasil pekerjaan

--- resources/views/pdf/pembayaran/penerimaan-hasil-pekerjaan.blade.php
+++ resources/views/pdf/pembayaran/penerimaan-hasil-pekerjaan.blade.php
@@ -7,68 +7,69 @@
 <body style="font-family: serif; line-height: 1.6; font-size: 14px; margin: 40px 40px 40px 100px;">
 
     <h2 style="text-align: center; text-transform: uppercase;">BERITA ACARA PENERIMAAN HASIL PEKERJAAN</h2>
-    <p style="text-align: center;">NOMOR ……………………</p>
+    <p style="text-align: center;">NOMOR {{$pemberitahuan->no_pbj}}/PHP/{{Auth::user()->kode_desa}}/{{Auth::user()->tahun_anggaran}}</p>
 
-    <p>Pada hari ini …………… tanggal ……….. bulan ………… tahun …….. bertempat di ………, telah dilaksanakan pembayaran atas pekerjaan ………… antara :</p>
+    <p style="text-align: justify">Pada hari ini …………… tanggal ……….. bulan ………… tahun {{Terbilang::make(Auth::user()->tahun_anggaran)}} bertempat di Kantor Kepala Desa {{Auth::user()->desa}}, telah dilaksanakan pembayaran atas pekerjaan pengadaan material {{$kegiatan->kegiatan}} antara :</p>
 
-    <p><strong>I. PIHAK PERTAMA</strong></p>
+    <p style="text-align: justify"><strong>I. PIHAK PERTAMA</strong></p>
     <table style="margin-left: 20px;">
         <tr>
             <td style="width: 120px;">Nama</td>
-            <td>: …………………………</td>
+            <td>: {{$kegiatan->pka}}</td>
         </tr>
         <tr>
             <td>Jabatan</td>
-            <td>: …………………………</td>
+            <td>: Pelaksana Kegiatan Anggaran</td>
         </tr>
         <tr>
             <td>Alamat</td>
-            <td>: …………………………</td>
+            <td>: Desa {{ Auth::user()->desa }} Kec. Pecalungan</td>
         </tr>
     </table>
 
-    <p><strong>II. PIHAK KEDUA</strong></p>
+    <p style="text-align: justify"><strong>II. PIHAK KEDUA</strong></p>
     <table style="margin-left: 20px;">
         <tr>
             <td style="width: 120px;">Nama</td>
-            <td>: …………………………</td>
+            <td>: {{$penyedia->nama_pemilik}}</td>
         </tr>
         <tr>
             <td>Jabatan</td>
-            <td>: …………………………</td>
+            <td>: {{$penyedia->jabata_pemilik}} {{$penyedia->nama_penyedia}}</td>
         </tr>
         <tr>
             <td>Alamat</td>
-            <td>: …………………………</td>
+            <td>: {{$penyedia->alamat_pemilik}}</td>
         </tr>
     </table>
 
-    <p>PIHAK PERTAMA menyatakan bahwa telah menerima hasil pekerjaan berupa ………………. dalam keadaan baik dari PIHAK KEDUA sesuai dengan Surat Perjanjian Nomor: ………………… dan Nomor ………….. tertanggal ……………….</p>
+    <p style="text-align: justify">PIHAK PERTAMA menyatakan bahwa telah menerima hasil pekerjaan berupa Material kegiatan {{$kegiatan->kegiatan }} dalam keadaan baik dari PIHAK KEDUA sesuai dengan Surat Perjanjian Nomor: …………………  tertanggal ……………….</p>
 
-    <p>PIHAK KEDUA telah menyerahkan hasil pekerjaan berupa ………… dalam keadaan baik kepada PIHAK PERTAMA sesuai dengan Surat Perjanjian Nomor ……… dan Nomor ……… tertanggal ……………….</p>
+    <p style="text-align: justify">PIHAK KEDUA telah menyerahkan hasil pekerjaan berupa Material kegiatan {{$kegiatan->kegiatan }} dalam keadaan baik kepada PIHAK PERTAMA sesuai dengan Surat Perjanjian Nomor ……… tertanggal ……………….</p>
 
-    <p>Demikian Berita Acara ini dibuat rangkap 2 (dua) masing-masing bermeterai cukup dan mempunyai kekuatan hukum yang sama untuk dipertanggungjawabkan sesuai peraturan perundang-undangan yang berlaku.</p>
+    <p style="text-align: justify">Demikian Berita Acara ini dibuat rangkap 2 (dua) masing-masing bermeterai cukup dan mempunyai kekuatan hukum yang sama untuk dipertanggungjawabkan sesuai peraturan perundang-undangan yang berlaku.</p>
 
     <br><br>
 
     <table style="width: 100%; text-align: center;">
         <tr>
-            <td>Direktur/Pimpinan/Pemilik</td>
+            <td>{{$penyedia->jabata_pemilik}} {{$penyedia->nama_penyedia}}</td>
             <td>Pelaksana Kegiatan Anggaran</td>
         </tr>
         <tr>
-            <td style="padding-top: 60px;">………………………….</td>
-            <td style="padding-top: 60px;">………………………….</td>
+            <td style="padding-top: 60px;">{{$penyedia->nama_pemilik}}</td>
+            <td style="padding-top: 60px;">{{$kegiatan->pka}}</td>
         </tr>
     </table>
 
     <br><br>
-
-    <p>Mengetahui</p>
-    <p>Kepala Desa …………….</p>
-    <p>Selaku Pemegang Kekuasaan Pengelolaan Keuangan Desa</p>
-    <br><br>
-    <p style="text-align: left;">……………………………………………</p>
+    <div style="text-align: center; line-height:1">
+        Mengetahui,<br>
+        Kepala Desa {{Auth::user()->desa}}<br>
+        Selaku Pemegang Kekuasaan Pengelolaan <br>Keuangan Desa<br>
+        <br><br><br><br><br>
+        <b>{{Auth::user()->kepala_desa}}</b><br>
+    </div>
 
 </body>
 </html>
