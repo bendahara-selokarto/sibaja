e0ccdf922d12b030532138d100cca6383a3950b5
harmanto <bendaharadesaselokarto@gmail.com>
2025-07-11 17:09:02 +0700

    koreksi surat-perjanjian-kerja

--- resources/views/pdf/negosiasi-harga/surat-perjanjian-kerja.blade.php
+++ resources/views/pdf/negosiasi-harga/surat-perjanjian-kerja.blade.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Surat Perjanjian Kerja</title>    
+    <style>
+        .pasal {
+            text-align: center;
+        }
+        table   {
+            border-collapse: collapse;
+        }
     </style>
 </head>
 <body style="margin: 0px 20px 20px 80px ;">
@@ -12,83 +19,135 @@
 <p style="margin-top: 0px; text-align:center">NOMOR : ..... / SPjK/{{ Auth::user()->kode_desa}}/{{Auth::user()->tahun_anggaran }}
     <br>TANGGAL : {{tanggal_indo($data['negosiasiHarga']->tgl_perjanjian)}}</p>
 
-<p>Pada hari ini {{$data['negosiasiHarga']->tgl_perjanjian->isoFormat('dddd')}} tanggal {{ucwords(Terbilang::make($data['negosiasiHarga']->tgl_perjanjian->isoFormat('D')))}} bulan {{$data['negosiasiHarga']->tgl_perjanjian->isoFormat('MMM')}} tahun {{Auth::user()->tahun_anggaran}} Kami yang bertanda tangan di bawah ini:</p>
+<p>Pada hari ini {{$data['negosiasiHarga']->tgl_perjanjian->isoFormat('dddd')}} tanggal {{ucwords(Terbilang::make($data['negosiasiHarga']->tgl_perjanjian->isoFormat('D')))}} bulan {{$data['negosiasiHarga']->tgl_perjanjian->isoFormat('MMMM')}} tahun {{Auth::user()->tahun_anggaran}} Kami yang bertanda tangan di bawah ini:</p>
+<table table style="width: 100%">
+    <tr>
+        <td rowspan="4" style="vertical-align: top">1.</td>
+        <td style="width:30mm">Nama</td>
+        <td style="width: 4pt">: </td>
+        <td>{{ $data['kegiatan']->pka}} </td>
+    </tr>
+    <tr>
+        <td>Jabatan</td>
+        <td>: </td>
+        <td>Pelaksana Kegiatan Angaran </td>
+    </tr>
+    <tr>
+        <td>Alamat</td>
+        <td>: </td>
+        <td>Desa {{Auth::user()->desa }} Kec. Pecalungan</td>
+    </tr>
+    <tr>        
+        <td colspan="4">
+                <em>Selanjutnya dalam hal ini disebut PIHAK PERTAMA </em>
+        </td>
+    </tr>
+</table>
+<br><br>
+<table style="width: 100%">
+    <tr>
+        <td rowspan="4" style="vertical-align: top">2.</td>
+        <td style="width:30mm">Nama</td>
+        <td style="width: 4pt">: </td>
+        <td>{{ $data['penyedia']->nama_pemilik}}</td>
+    </tr>
+    <tr>
+        <td>Jabatan</td>
+        <td>: </td>
+        <td>{{ $data['penyedia']->jabata_pemilik}} {{$data['penyedia']->nama_penyedia}}</td>
+    </tr>
+    <tr>
+        <td>Alamat</td>
+        <td>: </td>
+        <td>{{$data['penyedia']->alamat_pemilik}}</td>
+    </tr>
+    <tr>        
+        <td colspan="4">
+            <em>Selanjutnya dalam hal ini disebut PIHAK KEDUA </em>
+        </td>
+    </tr>
+</table>
+<br><br>
 
-<ol type="1">
-    <li>
-        Nama: …………………….<br>
-        Jabatan: Pelaksana Kegiatan Anggaran<br>
-        Alamat: …………………….<br>
-        <em>Selanjutnya disebut PIHAK PERTAMA</em>
-    </li>
-    <li>
-        Nama: …………………….<br>
-        Jabatan: …………………….<br>
-        Alamat: …………………….<br>
-        <em>Selanjutnya disebut PIHAK KEDUA</em>
-    </li>
-</ol>
-
-<div class="pasal">Pasal 1 – TUGAS PEKERJAAN</div>
-<p>PIHAK PERTAMA memberikan pekerjaan kepada PIHAK KEDUA dan PIHAK KEDUA menerima dan menyatakan bersedia melaksanakan pekerjaan:</p>
-<ul>
-    <li>Jenis pekerjaan: ……………………. </li>
-    <li>Lokasi pekerjaan: ……………………. </li>
-</ul>
-
-<div class="pasal">Pasal 2 – NILAI PEKERJAAN</div>
+<div class="pasal">Pasal 1 <br>TUGAS PEKERJAAN</div>
+<p>PIHAK PERTAMA memberikan pekerjaan kepada PIHAK KEDUA dan PIHAK KEDUA menerima dan menyatakan bersedia, setuju dan sanggup untuk melaksanakan pekerjaan:</p>
+<table>
+    <tr>
+        <td>1. Jenis Pekerjaan</td>
+        <td>: Pengadaan Material</td>
+    </tr>
+    <tr>
+        <td>2. Lokasi Pekerjaan</td>
+        <td>: Desa {{Auth::user()->desa}}</td>
+    </tr>
+</table>
+<br>
+<div class="pasal">Pasal 2 <br> NILAI PEKERJAAN</div>
 <p>Nilai pekerjaan yang disepakati oleh kedua pihak sebesar:</p>
 
-<table style="width: 100%">
+<table style="width: 100%; padding: 5px">
     <thead>
         <tr>
-            <th style="border-color:1px solid black">No</th>
-            <th style="border-color:1px solid black">Jenis Pekerjaan</th>
-            <th style="border-color:1px solid black">Banyaknya</th>
-            <th style="border-color:1px solid black">Satuan (Rp)</th>
-            <th style="border-color:1px solid black">Jumlah (Rp)</th>
+            <th style="border:1px solid black; width:5%">No</th>
+            <th style="border:1px solid black; width:35%">Jenis Pekerjaan <br> Yang Dikerjakan</th>
+            <th style="border:1px solid black; width:20%">Banyaknya</th>
+            <th style="border:1px solid black; width:20%">Harga Satuan <br>(Rp)</th>
+            <th style="border:1px solid black; width:20%">Jumlah <br>(Rp)</th>
         </tr>
     </thead>
     <tbody>
-        <tr><td colspan="5" style="height: 100px;"></td></tr>
         <tr>
-            <td colspan="4"><strong>Jumlah</strong></td>
-            <td>Rp ………………….</td>
+            <td style="border: 1px solid black; vertical-align:top; text-align:center">1</td>
+            <td style="border: 1px solid black; vertical-align:top">Pengadaan Material Kegiatan {{$data['kegiatan']->kegiatan}}</td>
+            <td style="border: 1px solid black; vertical-align:top">1 paket</td>
+            <td style="border: 1px solid black; vertical-align:top; text-align:right">{{(formatNumber(round($data['negosiasiHarga']->jumlah_total, -2)))}}</td>
+            <td style="border: 1px solid black; vertical-align:top; text-align:right">{{(formatNumber(round($data['negosiasiHarga']->jumlah_total, -2)))}}</td>
         </tr>
+        <tr>
+            <td colspan="4" style="border: 1px solid black; vertical-align:top; text-align:right"><strong>Jumlah</strong></td>
+            <td style="border: 1px solid black; vertical-align:top; text-align:right"><strong>{{(formatNumber(round($data['negosiasiHarga']->jumlah_total, -2)))}}</strong></td>
+        </tr>       
     </tbody>
 </table>
+<br><br>
+<div class="pasal">Pasal 3 <br> JANGKA WAKTU PELAKSANAAN</div>
+<p>Jangka waktu pelaksanaan adalah selama {{ $data['negosiasiHarga']->jumlah_hari_kerja }} ( {{Terbilang::make($data['negosiasiHarga']->jumlah_hari_kerja)}} ) hari kalender sejak tanggal {{ tanggal_indo($data['negosiasiHarga']->tgl_perjanjian) }} dan harus diselesaikan paling lambat tanggal {{ tanggal_indo($data['negosiasiHarga']->tgl_akhir_perjanjian) }}.</p>
 
-<div class="pasal">Pasal 3 – JANGKA WAKTU PELAKSANAAN</div>
-<p>Jangka waktu pelaksanaan adalah selama ............. (................) hari kalender sejak tanggal …………………., dan harus diselesaikan paling lambat tanggal ………………….</p>
-
-<div class="pasal">Pasal 4 – SERAH TERIMA PEKERJAAN</div>
+<div class="pasal">Pasal 4 <br> SERAH TERIMA PEKERJAAN</div>
 <ol type="1">
-    <li>Setelah pekerjaan selesai 100%, penyedia barang mengajukan penyerahan kepada PKA secara tertulis.</li>
-    <li>Pejabat pemeriksa melakukan pemeriksaan maksimal 7 hari setelah pengajuan.</li>
+    <li>Setelah pekerjaan selesai 100% (seratus per seratus), penyedia barang mengajukan penyerahan barang secara tertulis kepada Pelaksana Kegiatan Anggaran.</li>
+    <li>Pejabat pemeriksa hasil pekerjaan melakukan pemeriksaan terhadap hasil pekerjaan yang telah diselesaikan oleh penyedia barang selambat-lambatnya 7 (tujuh) hari setelah diterimanya surat permintaan dari penyedia barang. Selanjutnya dibuat berita acara pemeriksaan barang.</li>
+    <li>Apabila pada waktu serah terima barang dimaksud terdapat kekeliruan, tidak sesuai dan lain sebagainya, maka pihak kedua bersedia untuk memperbaiki sesuai dalam perencanaan yang tertuang dalam dokumen penunjukkan langsung.</li>
 </ol>
-<p>Jika terdapat kekeliruan saat serah terima, PIHAK KEDUA wajib memperbaiki sesuai dokumen penunjukan langsung.</p>
 
-<div class="pasal">Pasal 5 – CARA PEMBAYARAN</div>
+<div class="pasal">Pasal 5 <br> CARA PEMBAYARAN</div>
 <ol type="1">
     <li>Pembayaran 100% dilakukan setelah pekerjaan selesai dan dilampiri berita acara.</li>
-    <li>Pembayaran dilakukan melalui DPA Desa ………… untuk belanja modal pengadaan ……………. sebesar Rp ………………. ( ………………. ) dipotong pajak sesuai ketentuan.</li>
+    <li>Pembayaran dilakukan melalui DPA Desa {{Auth::user()->desa}} untuk belanja modal pengadaan Material kegiatan {{$data['kegiatan']->kegiatan}} kode rekening belanja {{$data['kegiatan']->rekening_apbdes}} secara Non Tunai / Transfer / CMS sejumlah nilai dalam kontrak sebesar Rp {{formatNumber(round($data['negosiasiHarga']->jumlah_total, -2))}} ( {{Terbilang::make(round($data['negosiasiHarga']->jumlah_total, -2))}} rupiah) dipotong pajak sesuai ketentuan Pemerintah.</li>
 </ol>
 
-<div class="pasal">Pasal 6 – HAK DAN KEWAJIBAN</div>
-<p><strong>Tim Pengelola Kegiatan:</strong></p>
-<ul>
-    <li>Mengawasi pekerjaan.</li>
-    <li>Meminta laporan periodik.</li>
-    <li>Menangguhkan pembayaran & mengenakan denda.</li>
-    <li>Memberikan instruksi & membayar nilai SPK.</li>
-</ul>
-
-<p><strong>Penyedia Barang:</strong></p>
-<ul>
-    <li>Menerima pembayaran sesuai nilai SPK.</li>
-    <li>Melaksanakan pekerjaan tepat waktu dan melaporkan secara periodik.</li>
-    <li>Menyerahkan hasil pekerjaan sesuai jadwal.</li>
-</ul>
+<div class="pasal">Pasal 6 <br> HAK DAN KEWAJIBAN</div>
+<ol>
+    <li>Hak dan Kewajiban Tim Pelaksana Kegiatan, sebagai berikut <br>
+        <ul type="a">
+            <li>Mengawasi pekerjaan yang dilaksanakan oleh penyedia barang.</li>
+            <li>Meminta laporan secara periodik mengenai pelaksanaan pekerjaan yang dilakukan oleh penyedia barang.</li>
+            <li>Menangguhkan pembayaran </li>
+            <li>Mengenakan denda keterlambatan.</li>
+            <li>Membayar nilai SPK kepada penyedia barang. </li> 
+            <li>Memberikan instruksi sesuai jadwal.</li>
+        </ul>    
+    </li>
+    <li>Hak dan Kewajiban Penyedia Barang, sebagai berikut: <br>
+        <ul type="a">
+            <li>Menerima pembayaran sesuai nilai SPK.</li>
+            <li>Menerima pembayaran ganti rugi / kompensasi (bila ada).</li>
+            <li>Melaksanakan dan menyelesaikan pekerjaan sesuai dengan jadwal pelaksanaan pekerjaan yang telah ditetapkan dalam kontrak.</li>
+            <li>Melaporan pelaksanaan pekerjaan secara periodik kepada Tim Pelaksana Kegiatan</li>
+            <li>Menyerahkan hasil pekerjaan sesuai jadwal penyerahan yang telah ditetapkan dalam SPK.</li>
+        </ul>
+    </li>
+</ol>
 
 <div class="pasal">Pasal 7 – SANKSI DAN DENDA</div>
 <ol type="1">
