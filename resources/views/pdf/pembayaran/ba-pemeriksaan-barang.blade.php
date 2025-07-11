b0e7c8ecacd7bd34115756431a8e456da4becd84
bendahara-selokarto <bendaharadesaselokarto@gmail.com>
2025-07-12 03:23:22 +0700

    koreksi ba pemeriksaan barang/jasa

--- resources/views/pdf/pembayaran/ba-pemeriksaan-barang.blade.php
+++ resources/views/pdf/pembayaran/ba-pemeriksaan-barang.blade.php
@@ -6,12 +6,52 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <h3 style="text-align: center;text-decoration:underline;">BERITA ACARA PEMERIKSAAN BARANG/PEKERJAAN</h3>
-
+<body style="margin: 60px 40px 40px 100px; line-height:1.5">
+    <h3 style="text-align: center;text-decoration:underline;">BERITA ACARA PEMERIKSAAN BARANG/PEKERJAAN</h3><br>
+    <p>Pada hari ini .... tanggal ………………….. bulan ………………… Tahun  {{Terbilang::make(Auth::user()->tahun_anggaran)}}   yang bertanda tangan dibawah ini  :</p>
     <table style="width: 100%">
         <tr>
-            <td>Rekanan</td>
+            <td style="width:6px">1.</td>
+            <td style="width: 50px">Nama</td>
+            <td>:</td>
+            <td>..........</td>
+            <td style="width:60px">Jabatan</td>
+            <td>:</td>
+            <td>Ketua</td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td>Nama</td>
+            <td>:</td>
+            <td>..........</td>
+            <td>Jabatan</td>
+            <td>:</td>
+            <td>Sekretaris</td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>Nama</td>
+            <td>:</td>
+            <td>..........</td>
+            <td>Jabatan</td>
+            <td>:</td>
+            <td>Anggota</td>
+        </tr>
+    </table>
+
+    <p style="text-align: justify">Berdasarkan Surat Keputusan Kepala Desa {{Auth::user()->desa}} Nomor : ………………. tanggal ……………….  Selaku Tim Pelaksana Kegiatan dan telah memeriksa Barang / Pekerjaan dengan teliti sebagai daftar terlampir yang telah diserahkan oleh : {{$penyedia->nama_pemilik ." ".$penyedia->jabata_pemilik ." ".$penyedia->nama_penyedia}}</p>
+    <p style="text-align: justify">Berdasarkan Surat Pesanan SPK Nomor : ……………………….. tanggal ...................................</p>
+    <p style="text-align: justify">Dengan kesimpulan sebagai berikut :</p>
+    <ol type="a">
+        <li>Terdapat baik sesuai dengan Surat SPK.</li>
+        <li>Kurang / Tidak baik.</li>
+    </ol>
+    <p style="text-align: justify">Barang yang kondisi baik yang kami beri tanda V yang selanjutnya akan diserahkan oleh rekanan kepada Tim Pengelola Kegiatan sedangkan yang tidak baik telah kami beri tanda X.</p>
+    <p style="text-align: justify">Demikian Berita Acara ini dibuat dalam  3 ( tiga ) rangkap untuk dipergunakan sebagai mana mestinya.</p>
+    
+    <table style="width: 100%">
+        <tr>
+            <td style="text-align: center">Rekanan</td>
             <td>1</td>
             <td>Nama</td>
             <td> ............. </td>
@@ -39,7 +79,7 @@
             <td></td>
         </tr>
         <tr>
-            <td> .................. </td>
+            <td style="width: 200px; text-align:center"> {{$penyedia->nama_pemilik}} </td>
             <td>3</td>
             <td>Nama</td>
             <td> ..... </td>
@@ -53,5 +93,6 @@
             <td></td>
         </tr>
     </table>
+    
 </body>
 </html>
