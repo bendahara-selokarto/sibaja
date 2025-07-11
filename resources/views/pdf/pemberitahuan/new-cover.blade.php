dfa52ad4a052ace7e7e3e178913e37703aefbca4
bendahara-selokarto <bendaharadesaselokarto@gmail.com>
2025-07-11 14:26:56 +0700

    koreksi new-cover

--- resources/views/pdf/pemberitahuan/new-cover.blade.php
+++ resources/views/pdf/pemberitahuan/new-cover.blade.php
@@ -1,22 +1,22 @@
-<div class="new-cover">
-        
-    <img src="batang.png" alt="batang" style="width:auto; height:4cm;">
-    <br><br>
+<div class="new-cover" style="position: relative;margin: 0; padding: 0;">
+
+   <div style="width: auto; height: 4cm; position: absolute; top: 60px; left: 0; right: 0; display: flex; align-items: center; justify-content: center;">
+    <img src="batang.png" alt="batang">
+    </div>
 
 
-        <h1 >DOKUMEN PENGADAAN</h1>
-        <h2 >BARANG / JASA</h2>
-        <h2 >DESA {{ strToUpper(Auth::user()->desa) }}</h2>
-        <h2 >KECAMATAN {{ strToUpper(Auth::user()->kecamatan) }}</h2>
-        <h2 >KABUPATEN BATANG</h2>
-        <br><br><br><br>
+
+        <h1 style="margin-top:420px">DOKUMEN <br> PENGADAAN BARANG / JASA</h1>
+        <br><br><br>
         
         
         <h1>KEGIATAN {{strToUpper($kegiatan->kegiatan)}}</h1>
-        <br><br><br><br><br><br><br>
-
-        <h2>PELAKSANAAN DANA DESA</h2>
-        <h2>KABUPATEN BATANG PROVINSI JAWA TENGAH</h2>
-        <h2>TAHUN ANGGARAN {{ strToUpper(Auth::user()->tahun_anggaran) }}</h2>
-
+        <br><br><br><br><br><br><br><br><br>
+        <div style="position: absolute; bottom: 40px; width:100%;">            
+            <h2>PELAKSANAAN DANA DESA</h2>
+            <h2 >DESA {{ strToUpper(Auth::user()->desa) }}</h2>
+            <h2 >KEC. {{ strToUpper(Auth::user()->kecamatan) }} KAB. BATANG</h2>
+            <h2>TAHUN ANGGARAN {{ strToUpper(Auth::user()->tahun_anggaran) }}</h2>
+        </div>
+            
 </div> 
