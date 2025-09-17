1acf7a705a65bb7b9e00bb7cf1f2061c53dbf9c8
harmanto <bendaharadesaselokarto@gmail.com>
2025-09-17 12:51:49 +0700

    menggunakan sumberdana pada cover

--- resources/views/pdf/pemberitahuan/new-cover.blade.php
+++ resources/views/pdf/pemberitahuan/new-cover.blade.php
@@ -14,7 +14,7 @@
         <h1 style="margin: 60px">KEGIATAN {{strToUpper($kegiatan->kegiatan)}}</h1>
         <br><br><br><br><br><br><br><br><br>
         <div style="position: absolute; bottom: 40px; width:100%;">            
-            <h2>PELAKSANAAN DANA DESA</h2>
+            <h2>PELAKSANAAN {{strToUpper($kegiatan->sumber_dana)}}</h2>
             <h2 >DESA {{ strToUpper(Auth::user()->desa) }}</h2>
             <h2 >KEC. {{ strToUpper(Auth::user()->kecamatan) }} KAB. BATANG</h2>
             <h2>TAHUN ANGGARAN {{ strToUpper(Auth::user()->tahun_anggaran) }}</h2>
