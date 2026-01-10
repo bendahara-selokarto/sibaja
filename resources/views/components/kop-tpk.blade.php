0208ab3275dac14ce77585c9f0e818f58df2fed2
bendahara-selokarto <bendaharadesaselokarto@gmail.com>
2026-01-10 10:19:59 +0700

    fix: correct spelling of "PEALUNGAN" to "PECALUNGAN" in kop-tpk component

--- resources/views/components/kop-tpk.blade.php
+++ resources/views/components/kop-tpk.blade.php
@@ -4,7 +4,7 @@
             <td style="text-align: center">
                 <h3 style="text-align: center; margin: 2pt;">TIM PELAKSANA KEGIATAN</h3>
                 <h3 style="text-align: center; margin: 2pt;">DESA {{ strToUpper( Auth::user()->desa )}}</h3>
-                <h4 style="text-align: center; margin: 2pt;">KECAMATAN PEALUNGAN</h4>
+                <h4 style="text-align: center; margin: 2pt;">KECAMATAN PECALUNGAN</h4>
                 <p  style="text-align: center; margin: 2pt;">{{ Auth::user()->alamat_kantor }}</p>
             </td>
         </tr>       
